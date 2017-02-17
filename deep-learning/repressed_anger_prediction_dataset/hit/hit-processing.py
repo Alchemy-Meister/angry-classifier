fdb4e8eb37c3c82a15f29cbfebf328877aea2186
Alchemy-Meister <jesusesma@gmail.com>
2017-02-17 18:54:12 +0900

    added original csv to hit processing

--- deep-learning/repressed_anger_prediction_dataset/hit/hit-processing.py
+++ deep-learning/repressed_anger_prediction_dataset/hit/hit-processing.py
@@ -2,18 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import getopt
+import itertools as it
 import numpy as np
+import operator
 import os
 import pandas as pd
+from random import randint
 import sys
-import codecs
-
-USAGE_STRING = 'Usage: cnn.py [-h] [--help] --target=[train, test, all] ' \
-    + '--train=path_to_train_word2vec ' \
-    + '--validation=path_to_validation_word2vect ' \
-    + '[--test=path_to_test_word2vec]' \
-    + '[--distribution=path_to_distribution_file] ' \
-    + '[--load_model=path_to_model] [--load_weights=path_to_weights]'
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 CWD = os.getcwd()
@@ -28,6 +23,39 @@ CLASSIFICATION_COLUMNS = [label.lower() for label in LABELS]
 IRONY_LABELS = ['Yes', 'No']
 IRONY_COLUMNS = [label.lower() for label in IRONY_LABELS]
 
+CLASSIFICATION_RESULT_COLUMN = ['manual_label']
+IRONY_RESULT_COLUMN = ['manual_irony']
+
+CSV_COLUMNS = ['tweet_id', 'label', 'author', 'content']
+
+# Author column is not required.
+COMPULSORY_COLUMNS = list(CSV_COLUMNS)
+del COMPULSORY_COLUMNS[2]
+
+def check_valid_path(path, desc):
+    if not os.path.isabs(path):
+        # Make relative path absolute.
+        path = os.path.join(CWD, path)
+
+    if not os.path.isfile(path):
+        print 'Error: Invalid %s file.' % desc
+        sys.exit(2)
+
+    return path
+
+def check_valid_dir(dir_name):
+    if not os.path.isabs(dir_name):
+        dir_name = os.path.join(CWD, dir_name)
+
+    if not os.path.exists(dir_name):
+        try:
+            os.makedirs(dir_name)
+        except:
+            print 'Error: Output directory file could not be created.'
+            print dir_name
+            sys.exit(2)
+
+    return dir_name
 
 def generate_frequency_df(columns, df):
     result_df = pd.DataFrame(columns=columns)
@@ -48,14 +76,26 @@ def generate_frequency_df(columns, df):
     return result_df
 
 def main(argv):
+    if(len(argv) != 1):
+        print 'Error, original dataset csv file path is required.'
+        sys.exit(2)
+
+    dataset_path = check_valid_path(argv[0], 'original csv')
+
+    output = dataset_path.split('.csv')[0] + '_hit_processed.csv'
+
+    print output
+
     classification_result_df = pd.DataFrame(columns=CLASSIFICATION_COLUMNS)
     irony_result_df = pd.DataFrame(columns=IRONY_COLUMNS)
 
     df_result_list = [classification_result_df, irony_result_df]
     column_list = [CLASSIFICATION_COLUMNS, IRONY_COLUMNS]
+    result_column_list = [CLASSIFICATION_RESULT_COLUMN, IRONY_RESULT_COLUMN]
 
+    irony_idxs = []
 
-    for file in os.listdir(SCRIPT_DIR):
+    for file_index, file in enumerate(os.listdir(SCRIPT_DIR)):
         if file.endswith('.csv'):
             # print file
             # Loads CSV into dataframe.
@@ -75,8 +115,14 @@ def main(argv):
             # print irony_df.columns
 
             # Get the tweet original position for the irony column.
-            # for column in irony_df.columns:
-            #    print df.columns.get_loc(column) - 1
+            irony_idx = [file_index * 100 + df.columns.get_loc(column) \
+                for column in irony_df.columns ]
+
+            # Fix the deviation of irony index.
+            for index, idx in enumerate(irony_idx):
+                irony_idx[index] -= 1 + index
+
+            irony_idxs.extend(irony_idx)
 
             df_list = [classification_df, irony_df]
 
@@ -88,7 +134,39 @@ def main(argv):
 
     for index, df in enumerate(df_result_list):
         df[column_list[index]] = df[column_list[index]].astype(int)
-        print df
+        rowmax = df.max(axis=1)
+        idx = np.where(df.values == rowmax[:,None])
+
+        groups = it.groupby(zip(*idx), key=operator.itemgetter(0))
+
+        rowmax = [[df.columns[j] for i, j in grp] for k, grp in groups]
+
+        df = pd.DataFrame(columns=result_column_list[index])
+
+        for classification in rowmax:
+            max_length = len(classification)
+            max_label = {}
+            if max_length == 1:
+                max_label[result_column_list[index][0]] = classification[0]
+            else:
+                max_label[result_column_list[index][0]] = classification[ \
+                    randint(0, max_length - 1) ]
+
+            df = df.append(max_label, ignore_index=True)
+
+        df_result_list[index] = df
+
+    # Load original dataset.
+    df = pd.read_csv(dataset_path, header=0, \
+        dtype={COMPULSORY_COLUMNS[0]: np.int64})
+
+    df = pd.concat([df, df_result_list[0]], ignore_index=True, axis=1)
+
+    print df
+
+    print df_result_list[0]
+    df_result_list[1].index = irony_idxs
+    print df_result_list[1]
 
 if __name__ == '__main__':
     main(sys.argv[1:])
---
+++ experiments/repressed-anger/11.process_hit.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python ../../deep-learning/repressed_anger_prediction_dataset/hit/hit-processing.py \
+    ../../datasets/merged/repressed_anger_prediction_dataset.csv
